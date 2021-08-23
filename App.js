96271430fb0ec2f7b105bea641597a19e327a04f
jbrasko <jbrasko0508@gmail.com>
2021-08-23 16:25:35 -0500

    update imports

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
-import IndexScreen from './src/screens/IndexScreen'
 import { Provider as BlogProvider } from './src/context/BlogContext'
+import IndexScreen from './src/screens/IndexScreen'
+import ShowScreen from './src/screens/ShowScreen'
 
 const navigator = createStackNavigator(
   {
