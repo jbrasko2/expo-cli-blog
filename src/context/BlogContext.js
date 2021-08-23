fadc3e9258848b55d2d78c03b0a808dc15abda88
jbrasko <jbrasko0508@gmail.com>
2021-08-23 18:10:59 -0500

    add editBlogPost, back to show after submit

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -13,6 +13,10 @@ const blogReducer = (state, action) => {
       ]
     case 'delete_blog_post':
       return state.filter(blogPost => blogPost.id !== action.payload)
+    case 'edit_blog_post':
+      return state.map(blogPost => {
+        return blogPost.id === action.payload.id ? action.payload : blogPost
+      })
     default:
       return state
   }
@@ -31,11 +35,19 @@ const deleteBlogPost = dispatch => {
   }
 }
 
+const editBlogPost = dispatch => {
+  return (id, title, content, callback) => {
+    dispatch({ type: 'edit_blog_post', payload: { id, title, content } })
+    callback()
+  }
+}
+
 export const { Context, Provider } = createDataContext(
   blogReducer,
   {
     addBlogPost,
     deleteBlogPost,
+    editBlogPost,
   },
   [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
 )
--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -4,17 +4,19 @@ import { Context } from '../context/BlogContext'
 import BlogPostForm from '../components/BlogPostForm'
 
 const EditScreen = ({ navigation }) => {
-  const { state } = useContext(Context)
-  const blogPost = state.find(
-    blogPost => blogPost.id === navigation.getParam('id')
-  )
+  const id = navigation.getParam('id')
+  const { state, editBlogPost } = useContext(Context)
+
+  const blogPost = state.find(blogPost => blogPost.id === id)
 
   return (
     <BlogPostForm
-      onSubmit={(title, content) => {
-        console.log(title, content)
-      }}
       initialValues={{ title: blogPost.title, content: blogPost.content }}
+      onSubmit={(title, content) => {
+        editBlogPost(id, title, content, () =>
+          navigation.navigate('Show', { id: id })
+        )
+      }}
     />
   )
 }
