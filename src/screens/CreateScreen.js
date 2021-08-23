57e7edc390229dc66737e793dd914f48e5cf8757
jbrasko <jbrasko0508@gmail.com>
2021-08-23 16:57:27 -0500

    start create form, add styling

--- src/screens/CreateScreen.js
+++ src/screens/CreateScreen.js
@@ -1,15 +1,43 @@
-import React, { useContext } from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useContext } from 'react'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
 import { Context } from '../context/BlogContext'
 
 const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
+
   return (
     <View>
-      <Text>Create</Text>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        value={title}
+        onChangeText={text => setTitle(text)}
+        style={styles.input}
+      />
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput
+        value={content}
+        onChangeText={text => setContent(text)}
+        style={styles.input}
+      />
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginHorizontal: 5,
+  },
+})
 
 export default CreateScreen
