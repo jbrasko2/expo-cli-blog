0888c8462089faed8f75bbf37dcafa294a51c299
jbrasko <jbrasko0508@gmail.com>
2021-08-24 17:34:08 -0500

    add didFocus listener

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -16,6 +16,14 @@ const IndexScreen = ({ navigation }) => {
 
   useEffect(() => {
     getBlogPosts()
+
+    const listener = navigation.addListener('didFocus', () => {
+      getBlogPosts()
+    })
+
+    return () => {
+      listener.remove
+    }
   }, [])
 
   return (
